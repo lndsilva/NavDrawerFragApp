3acf7c8ad736ec9f8df237cb054a9c22dcd9eb77
lndsilva <lndsilva@hotmail.com>
2025-09-19 14:43:37 -0300

    drawer layout

--- app/src/main/java/br/com/local/navdrawerfragapp/MainActivity.java
+++ app/src/main/java/br/com/local/navdrawerfragapp/MainActivity.java
@@ -1,12 +1,19 @@
 package br.com.local.navdrawerfragapp;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +27,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        MaterialToolbar toolbar = findViewById(R.id.idTopAppBar);
+        DrawerLayout drawerLayout = findViewById(R.id.main);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
     }
 }
